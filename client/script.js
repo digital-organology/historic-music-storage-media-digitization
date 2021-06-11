4f9dd0cecc499295d43c2a30942a4a7f07e7d102
David Fuhry <df15nocu@studserv.uni-leipzig.de>
2021-06-11 14:50:59 +0200

    Update client side stuff

--- client/script.js
+++ client/script.js
@@ -1,22 +1,192 @@
 // Creates canvas 320 × 200 at 10, 50
 var paper = Raphael(0, 0, 4000, 3500);
 
+function onNoteClick(e) {
+    debugger;
+    alert("Note ID: ".concat(e.target.id).concat("; Track ID: ").concat(e.target.dataset.track));
+}
 
-fetch("test_data.json").then(res => res.json()).then(data => {
-    arr = data["data"];
+const CSS_COLOR_NAMES = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+  ];
 
- 
-    for (var obj of arr) {
+function hightlightNote(e) {
+    track = e.target.dataset.track;
+    trackElements = document.querySelectorAll('[data-track="' + track + '"]');
+    trackElements.forEach(node => {
+        node.classList.remove("inactive");
+        node.classList.add("active");
+    })
+}
 
-        var id = obj["id"];
-        console.log(id);
-        var track = obj["track"];
-        var points = obj["points"];
+function lowlightNote(e) {
+    track = e.target.dataset.track;
+    trackElements = document.querySelectorAll('[data-track="' + track + '"]');
+    trackElements.forEach(node => {
+        node.classList.remove("active");
+        node.classList.add("inactive");
+    })
+}
 
-        points.forEach(function (point, idx) {
+fetch("data.json")
+    .then(res => res.json())
+    .then(data => {
+        data["data"].forEach(shape => {
+            var note = paper.path(shape.points)
+                            .attr({"fill" : CSS_COLOR_NAMES[shape.track]});
+            node_id = "shape_".concat(shape.id);
+            note.node.id = node_id;
+            note.node.dataset.track = shape.track;
+            $("#".concat(node_id)).click(onNoteClick);
+            $("#".concat(node_id)).on("mouseenter", hightlightNote);
+            $("#".concat(node_id)).on("mouseout", lowlightNote);
 
-            var rect = paper.rect(point[0], point[1], 0.001, 0.001);
-            rect.attr("stroke", "green");
-        });
-    };
+        })
 });
